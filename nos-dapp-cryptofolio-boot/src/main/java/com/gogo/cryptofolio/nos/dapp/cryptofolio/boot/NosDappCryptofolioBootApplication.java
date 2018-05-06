ea489d1aff1ddf0f1554aa79a70f5eef018e00c9
Robbert Klein <robbert743@gmail.com>
2018-05-06 21:26:43 +0200

    project setup

---
+++ nos-dapp-cryptofolio-boot/src/main/java/com/gogo/cryptofolio/nos/dapp/cryptofolio/boot/NosDappCryptofolioBootApplication.java
@@ -0,0 +1,12 @@
+package com.gogo.cryptofolio.nos.dapp.cryptofolio.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosDappCryptofolioBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosDappCryptofolioBootApplication.class, args);
+    }
+}
