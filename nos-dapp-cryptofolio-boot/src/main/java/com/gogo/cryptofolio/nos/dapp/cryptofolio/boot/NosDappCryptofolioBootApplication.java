c57fa3102e8c71d0c803f96b0b683c4734c52a54
Robbert Klein <robbert743@gmail.com>
2018-05-14 21:18:18 +0200

    [develop] add multi-module setup

--- nos-dapp-cryptofolio-boot/src/main/java/com/gogo/cryptofolio/nos/dapp/cryptofolio/boot/NosDappCryptofolioBootApplication.java
+++ nos-dapp-cryptofolio-boot/src/main/java/com/gogo/cryptofolio/nos/dapp/cryptofolio/boot/NosDappCryptofolioBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NosDappCryptofolioBootApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(NosDappCryptofolioBootApplication.class, args);
     }
 }
